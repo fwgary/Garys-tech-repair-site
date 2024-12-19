4f0f7c92bb4ea38826c360107063f2a0655d2b0a
fwgary <gkent7094@gmail.com>
2024-12-18 21:18:09 -0500

    ad

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from website import create_app, db
-from website.models import User
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from flask_login import login_required, current_user
-from flask_socketio import SocketIO, emit
-import socket
+from flask import Flask, render_template, request, redirect, url_for
+import requests
 
-app = create_app()
-socketio = SocketIO(app)
+app = Flask(__name__)
+
+DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1319125949446361088/Ip0Xv_tormJ_No6aZcxDdqt1ZZ8tpFamjkN5Bp4EZf3w1Qvwer6kENXTgiIJfPAN4pTK'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/devices')
+def devices():
+    return render_template('devices.html')
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    if request.form.get('agree'):
+        # Send a POST request to Discord webhook
+        requests.post(DISCORD_WEBHOOK_URL, json={"content": "A new order has been placed!"})
+        return redirect(url_for('home'))
+    return redirect(url_for('devices'))
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
 
 if __name__ == '__main__':
-  socketio.run(app,
-                 host='0.0.0.0',
-                 port=81,
-                 debug=True,
-                 use_reloader=True,
-                 log_output=True)
+    app.run(debug=True)
